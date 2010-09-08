1f83e640a2d524f0713a9a075cb2f8fa2d0bc94e
Rebecca Meritz <rjmeritz@gmail.com>
2010-09-08 18:50:14 -0400

    Ch 7 & ch8

--- Ch7ex.hs
+++ Ch7ex.hs
@@ -1,7 +1,7 @@
 --The Craft of Functional Programming 
 --Ch. 7 Defining Functions Over Lists
 
-import Prelude hiding (product, and, or, reverse, unzip, drop, splitAt, take, zip3)
+import Prelude hiding (product, and, or, reverse, unzip, drop, take, zip3,getLine)
 
 --7.1
 
@@ -14,7 +14,7 @@ frtintplusone (x:xs) = x + 1
 frsttwo:: [Int] -> Int
 frsttwo [] = 0
 frsttwo [x] = x
-frsttwo(x:xs) = x + (x:xs)!!1
+frsttwo(x:y:xs) = x + y
 
 --7.3
 
@@ -135,18 +135,19 @@ iSortNoDuplicate  []     = []
 iSortNoDuplicate  (x:xs) = insRemove x (iSortNoDuplicate  xs)
 
 --7.13
+{-want to talk in person
+ --(x,y):(z,w):[(zs,ws)]
 
-{- Why don't I work ? Types...
 insPair :: (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
 insPair (x,y) [] = [(x,y)]
-insPair (x,y) ((z,w):(zs,ws))
- |x < z = (x,y):[(z,w):(zs,ws)]
- |x == z && y < w  = (x,y):[(z,w):(zs,ws)]
+insPair (x,y) (z,w):[(zs,ws)]
+ |x < z = (x,y):(z,w):[(zs,ws)]
+ |(x == z) && (y < w ) = (x,y):(z,w):(zs,ws)
  |otherwise = (z,w): (insPair (x,y) (zs,ws))
 
 pairISort :: [(Int, Int)] -> [(Int, Int)]
 pairISort []     = [] 
-pairISort ((x,y):(xs,ys)) = insPair (x,y) (pairISort (xs,ys))
+pairISort (x,y):[(xs,ys)] = insPair (x,y) (pairISort [(xs,ys)])
 -}
 
 --7.14
@@ -158,6 +159,7 @@ drop n (x:xs)
   |n==0 =(x:xs)
   |n<0 = error "PreludeList.drop: negative argument"
 
+--want to talk with you actualy
 {-More issues with reccursion on paired types
 splitAt ::Int -> [a] -> ([a],[a])
 splitAt _ [] = ([],[])
@@ -185,7 +187,295 @@ zip3:: [a] -> [b] -> [c] -> [(a,b,c)]
 zip3 (x:xs) (y:ys) (z:zs) = (x,y,z):zip3 xs ys zs
 zip3 _ _ _ = []
 
+--want to talk with you actually 
 {-Even more issues making zip3 using zip
 nomatchzip3 ::[a] -> [b] -> [c] -> [(a,b,c)]
 nomatchzip3 x y z = (((zip x y)!!1), (z!!1))
 -}
+
+--7.17
+
+qSort :: [Int]-> [Int]
+qSort[] = []
+qSort (x:xs) = qSort [y | y <-xs, y<=x] ++ [x] ++ qSort[y |y <-xs, y>x]
+
+qSortDescend :: [Int] -> [Int]
+qSortDescend[] = []
+qSortDescend (x:xs) = qSortDescend [y | y <-xs, y>=x] ++ [x] ++ qSortDescend [y |y <-xs, y<x]
+
+qSortNoDuplicate :: [Int]-> [Int]
+qSortNoDuplicate[] = []
+qSortNoDuplicate (x:xs) = qSortNoDuplicate [y | y <-xs, y<x] ++ [x] ++ qSortNoDuplicate[y |y <-xs, y>x]
+
+--7.18
+
+--same as built-it function elem
+isElementOf :: (Eq a)=> a -> [a] -> Bool
+isElementOf _ []     = False
+isElementOf e (x:xs) =  e == x || e `isElementOf` xs
+
+sublist :: (Eq a)=> [a] -> [a] -> Bool 
+sublist [] ys = True
+sublist (x:xs) ys = (isElementOf x ys) && (sublist xs ys)
+
+--whichElement :: (Eq a)=> a ->[a] -> Int -> Int  
+whichElement _ [] _ = 0
+whichElement e (x:xs) n
+  |e==x = n
+  |otherwise = whichElement e xs (n+1)
+{-}
+--maybe instead of what I currently have for options I got use the splitAt functiont to divide the list into seperate lists and compare them 
+
+--options produces an infinate loop
+--is it bc n and p are defined in terms of each other
+--I thought you could do this like you do for odd and even      
+--options :: a -> [a] -> [a]
+options _ []= []
+options e xs 
+  |p==0 = []
+  |p/=0 = n : options e n
+  where
+  n=(drop p xs)
+  p=(whichElement e n 0)
+-}   
+subsequenceHelper [] _ = True 
+subsequenceHelper _ [] = False
+subsequenceHelper (x:xs) (y:ys) = (x==y) && (subsequenceHelper xs ys)
+
+{-
+--subsequence :: [a] -> [a] -> Bool
+--only works is first occurance of x is where subsquence is 
+subsequence (x:xs) ys
+  |sublist (x:xs) ys = subsequenceHelper (x:xs) (drop (whichElement x ys 0) ys)
+  |otherwise =False 
+
+--so even if I had a working options function I'm not sure how to right a program to effectively us it
+subsequence (x:xs) ys
+  |n /= [] = subsequenceHelper (x:xs) head n ++  
+  |otherwise =False 
+  where 
+  n = options x ys
+  l=(length (options x ys)-1) 
+-}
+-- I have a working subsequnce though I'd still like to know how to make my other idea work 
+subsequence _ []= False
+subsequence xs (y:ys) = subsequenceHelper xs (y:ys) || subsequence xs ys 
+
+  
+--7.19
+
+type Word = String 
+type Line = [Word]
+
+whitespace = ['\n', '\t',' ']
+
+getWord :: String -> String
+getWord [] = []
+getWord (x:xs)
+  |isElementOf x whitespace = []
+  |otherwise = x: getWord xs
+
+dropWord :: String -> String 
+dropWord [] = []
+dropWord (x:xs)
+  |isElementOf x whitespace = (x:xs)
+  |otherwise = dropWord xs
+  
+dropSpace :: String -> String
+dropSpace [] = []
+dropSpace (x:xs)
+  |isElementOf x whitespace = dropSpace xs
+  |otherwise = (x:xs)
+
+split :: String -> [Word]
+split [] = [] 
+split st = (getWord st): split (dropSpace (dropWord st))
+  
+splitWords :: String -> [Word]
+splitWords st = split (dropSpace st)
+
+getLine :: Int -> [Word]-> Line
+getLine len [] = []
+getLine len (w:ws)
+  |length w <=len = w: restOfLine
+  |otherwise = []
+  where
+  newlen = len - (length w + 1)
+  restOfLine = getLine newlen ws
+  
+dropLine :: Int ->  [Word] -> Line
+dropLine _ [] = []
+dropLine len (w:ws)
+  |length w <=len = restOfLines
+  |otherwise = (w:ws)
+  where 
+  newlen = len - (length w +1)
+  restOfLines = dropLine newlen ws 
+
+--test values
+lineLen:: Int
+lineLen = 15  
+
+ln ::[Word]
+ln = ["1", "234", "567", "8901234", "56"]
+
+someWords :: [Word]
+someWords =["I", "am", "not", "a", "very", "good", "typer", "but", "I", "need", "to", "type", "a", "lot", "to", "test", "my", "haskell", "program"]
+  
+splitLines :: [Word] -> [Line] 
+splitLines [] = []
+splitLines ws = getLine lineLen ws : splitLines (dropLine lineLen ws)  
+
+--7.20
+
+--Is there another way to eliminate the space at the end of the line? Do I have to use a helper function to look at the length?
+joinLineHelper :: Line -> Int-> String 
+joinLineHelper [] _ = ""
+joinLineHelper (w:ws) n 
+  |n> 1 =  w ++ " " ++joinLineHelper ws (n-1)
+  |otherwise = w
+  
+joinLine :: Line -> String 
+joinLine line =  joinLineHelper line l 
+  where
+  l = length line
+
+--7.21
+
+joinLines :: [Line] -> String
+joinLines [] = ""
+joinLines (l:ls) = joinLine l ++"\n"++ joinLines ls
+
+--7.22
+--Redine functions so they work like splitAt instead of take and drop but I need to unserstand splitAt first
+
+--7.23
+--its almost correct but not quiet it puts too many gaps after the first word
+--the line length isn't always right either
+--I think the concept it sound though
+--also doesn't work is only one word in line
+
+joinLineJustifyHelper :: Int -> Int -> Int -> Int -> Line -> String
+joinLineJustifyHelper e n ee nw (w:ws)
+ |nw==1 = w  
+ |(ee>= 1)= w ++ (joinLine (replicate ss " "))++ joinLineJustifyHelper e n (ee-1) (nw-1) ws
+ |otherwise  = w ++ (joinLine (replicate s " "))++ joinLineJustifyHelper e n 0 (nw-1) ws 
+  where
+  s = (e `div` n)   
+  ss = s+1
+
+joinLineJustify :: Line -> String
+joinLineJustify lns = joinLineJustifyHelper whiteSpacesTotal numberOfGaps extraWhiteSpaces numberOfWords lns 
+  where 
+  whiteSpacesTotal= lineLen - (length (joinLine lns)) + numberOfGaps 
+  numberOfGaps= numberOfWords - 1 
+  extraWhiteSpaces = whiteSpacesTotal `mod` numberOfGaps 
+  numberOfWords = length lns 
+
+joinLinesJustify :: [Line] -> String
+joinLinesJustify (l:ls)
+  |length (l:ls)> 1 = joinLineJustify l ++"\n"++ joinLinesJustify ls
+  |otherwise = joinLine l 
+
+--7.24
+numberOfChars ::String -> Int ->  Int
+numberOfChars "" n = n 
+numberOfChars (c:cs) n 
+  |elem c whitespace == False  =numberOfChars cs n+1
+  |otherwise = numberOfChars cs n  
+
+numberOfWords :: String -> Int 
+numberOfWords str = length (split str)
+
+numberOfLines:: String -> Int -> Int 
+numberOfLines "" n = n 
+numberOfLines (c:cs) n
+  |c=='\n' =numberOfLines cs (n+1)
+  |otherwise =numberOfLines cs n  
+
+wc :: String -> (Int, Int, Int)
+wc str = (numberOfChars str 0, numberOfWords str, numberOfLines str 0)
+
+numberOfLinesFormat :: String -> Int
+numberOfLinesFormat str = length (splitLines(splitWords str))
+
+wcFormat :: String -> (Int, Int, Int)
+wcFormat str = (numberOfChars str 0, numberOfWords str, numberOfLinesFormat str)
+
+--7.25
+---if I put ''' in charctures I get an error, how can I account fot ', I guess I could look up its ord?
+charctures = ['!','-', ':', ';', '.', '"', '`', ',', '?']
+
+ord :: Char -> Int
+ord  =  fromEnum
+
+chr  :: Int  -> Char
+chr  =  toEnum
+
+offset :: Int
+offset = ord 'A' - ord 'a'
+
+allLower :: String -> String
+allLower "" = "" 
+allLower (c:ch)
+  |elem c charctures || elem c whitespace  = allLower ch 
+  |ord c <= 90 = (chr(ord c - offset)): allLower ch 
+  |otherwise = c: allLower ch 
+
+isPalinHelper :: String -> Int ->  Bool 
+isPalinHelper "" _ = True
+isPalinHelper _ 1 = True
+isPalinHelper (c:cs) l = (c == ((c:cs)!!(l-1))) && (isPalinHelper (take (l-2) cs) (l-2))  
+
+isPalin :: String -> Bool 
+isPalin str = isPalinHelper newstr l 
+  where
+  newstr = allLower str
+  l= length newstr
+
+--7.26
+
+ 
+--Example data
+st, oldSub, newSub :: String 
+st= "How tall is that?"
+oldSub = "tall is"
+newSub = "much toothpaste"
+
+--Works if old is a word, but doesn't work for any string
+
+subsitute :: [Word] -> Word -> Word -> [Word]
+subsitute [] _ _ = []
+subsitute (c:cs) o n 
+  |(c==o) = n :subsitute cs o n 
+  |otherwise = c: subsitute cs o n  
+
+subWord :: String -> String -> String -> String 
+subWord string old new = joinLine(subsitute wordlist old new)  
+  where  
+  wordlist= splitWords string 
+
+subsequenceLocation :: String -> String -> Int -> [Int]
+subsequenceLocation _ "" _ = []
+subsequenceLocation old (s:ss) n 
+  |subsequenceHelper old (s:ss) = n: subsequenceLocation old ss (n+1) 
+  |otherwise = subsequenceLocation old ss (n+1) 
+
+frt (x,y) = x
+
+scd(x,y)=y
+
+--the type of restOfList  is [String] not String. How do I fix?
+
+subSt :: String -> String -> String -> String
+subSt "" _ _ = ""
+subSt st o n  
+  |length locate > 0 = beginning ++ n ++ subSt(restOfList o n)
+  |otherwise = st
+  where 
+  locate = subsequenceLocation o st 1
+  stLen = length o 
+  beginning = frt(splitAt (head locate) st) 
+  end = scd (splitAt (head locate) st)
+  restOfList = drop stLen end
+  
---
+++ Ch8ex.hs
@@ -0,0 +1,256 @@
+--The Craft of Functional Programming 
+--Ch. 8 Reasoning about Programs
+
+--8.1
+
+fact :: Int -> Int
+fact n
+  | n==0        = 1
+  | otherwise   = n * fact (n-1)
+
+--(4>2) || (fact(-1)==17) -- True 
+--(4>2) && (fact(-1)==17) --False
+
+--8.2
+
+mult :: Int -> Int -> Int
+mult 0 _ = 0 
+mult 1 n = n 
+mult x n = n + (mult (x-1) n) 
+
+--0 `mult` fact(-2) = 0 
+--fact(-2) `mult` 0 = undefined
+  
+
+--8.3
+{- Prove 
+sum (xs ++ ys) = sum xs + sum ys   (hyp)  (sum++)
+
+
+[] ++ zs = zs                      (++.1)
+(w:ws) ++ zs = w:(ws++zs)          (++.2)
+ 
+sum [] = 0                         (sum.1)
+sum (x:xs) = x + sum xs            (sum.2)
+
+(base)
+sum([]++ys)= sum(ys)              by(++.1) 
+
+sum [] + sum ys = 0 + sum ys      by(sum.1)
+=sum ys 
+
+(induction)
+sum((x:xs)++ys)
+= sum (x:(xs++ys))                by(++.2)
+=x + sum(xs++ys)                  by(sum.2)
+=x + sum xs + sum ys              by(hyp)
+
+sum (x:xs) + sum ys 
+= x + sum xs + sum                by(sum.2)
+-}
+
+--8.4
+{- Prove 
+xs ++ [] = xs                      (hyp) (++.3)
+
+(base)
+xs ++ [] = [] ++ []= []            by(++.1)
+
+xs = [] 
+
+(induction)
+(x:xs) ++ []
+=x:(xs++[])                        by(++.2)
+=x:xs                              by(hyp)
+ 
+(x:xs)
+-}
+
+{-Prove
+xs ++ (ys ++ zs) = (xs ++ys) ++ zs (hyp)  (++.4)
+
+(base)
+[] ++ (ys ++ zs)
+=(ys ++zs)                          by(++.1)
+
+([]++ys) ++ zs
+=ys ++ zs                           by(++.1)
+
+(induction)
+(x:xs) ++ (ys ++zs)
+=x:(xs++(ys++zs))                   by(++.2)
+=x:((xs ++ys) ++ zs)                by(hyp)
+
+((x:xs)++ys)++zs
+=(x:(xs++ys))++zs                   by(++.2)
+
+--They look really close by the parens don't quite match
+--do I need to show for finate ys and zs to complete the proof or is that just assigned as an exercise?
+-}
+
+--8.5
+
+{-Prove
+sum (reverse xs) = sum xs          (hyp)
+
+reverse [] = []                    (reverse.1)
+reverse (z:zs) = reverse zs ++ z   (reverse.2)
+
+(base)
+sum(reverse []) = sum ([])         by(reverse.1)
+=0                                 by (sum.1)
+
+sum [] = 0                         by (sum.1)
+
+(induction)
+sum(reverse (x:xs))
+= sum(reverse xs ++ [x])           by(reverse.2)
+= sum(reverse xs) + sum [x]        by(sum++)
+= sum(reverse xs) + x              by(sum.2)
+= sum xs + x                       by(hyp)
+
+sum(x:xs)
+= x + sum xs                       by(sum.2)
+= sum xs + x                       by associative property of addtion 
+
+-}
+
+{-Prove 
+length (reverse xs) = length xs
+
+length [] = 0                            (length.1)
+length (z:zs) = 1 + length zs            (length.2)
+length (xs++ys) = lenght xs + length ys  (length.4)
+
+(base) 
+length (reverse []) = length ([]) by(reverse.1)
+=0                                by(length.1)
+
+length [] = 0                     by(lenght.1)
+
+(induction)
+length (reverse(x:xs))
+=length(reverse xs ++ x)          by(reverse.2)
+=length(reverse xs) + length x    by(length.4)
+=length(reverse xs) + 1           by(length.2)
+=length(xs) + 1                   by(hyp)
+
+length(x:xs)
+=1 + length xs                    by(length.2)
+=length(xs) + 1                   by associative property of addtion
+
+-}
+
+--8.6
+
+{- Prove 
+elem z (xs ++ zs) = elem z xs || elem z zs   (hyp)
+
+elem x [] = False                            (elem.1)
+elem x (y:ys) = (x==y) || (elem x ys)        (elem.2)
+
+(base)
+elem z ([] ++ zs)
+elem z zs                                     by(++.1)
+
+elem z [] || elem z zs
+False || elem z zs                            by(elem.1)
+elem z zs                                     by ||
+
+(induction)
+elem z ((x:xs) ++ zs)
+elem z (x:(xs ++ zs))                         by(++.2)
+(z==x)||elem z (xs++zs)                       by(elem.2)
+(z==x)||elem z xs || elem z zs                by(hyp)
+
+elem z (x:xs) || elem z zs
+(z==x)||elem z xs || elem z zs                by(elem.2)
+
+-}
+
+--8.7
+{-Prove
+zip (frt (unzip ps)) (scd(unzip ps)) = ps
+
+unzip [] = ([],[])                            (unzip.1)
+unzip ((x,y):ps) = (x:xs,y:ys)                (unzip.2)
+  where (xs,ys)=unzip ps  
+  
+frt(x,y) = x                                  (frt.1)
+
+scd(x,y) = y                                  (scd.1)
+
+zip (x:xs) (y:ys) = (x,y) : zip xs ys         (zip.1)
+zip (x:xs) []     = []                        (zip.2)
+zip []     zs     = []                        (zip.3)
+
+(base)
+zip (frt (unzip [])) (scd(unzip []))         
+=zip (frt ([],[])) (scd([],[]))               by(unzip.1)
+=zip [] (scd([],[]))                          by(frt.1)
+=zip [] []                                    by(scd.1)
+=[]                                           by(zip.2)
+
+[]
+
+(induction)
+
+where 
+ps = (xs,ys)                       --can I just assume this form?
+p:ps = ((x,y):(xs,ys))
+
+zip (frt (unzip ((p:ps))) (scd(unzip ((p:ps)))
+zip (frt (unzip ((x,y):(xs,ys))) (scd(unzip ((x,y):(xs,ys)))
+zip (frt (x:xs,y:ys)) (scd(x:xs,y:ys))               by(unzip.2)
+zip (x:xs) (scd(x:xs,y:ys))                          by(frt)
+zip (x:xs) (y:ys)                                    by(scd)
+(x,y) : zip xs ys                                    by(zip.1)
+
+(p:ps)
+((x,y):(xs,ys))                                      
+zip (frt (unzip ((x,y):(xs,ys))) (scd(unzip ((x,y):(xs,ys))) by(hyp)
+zip (frt (x:xs,y:ys)) (scd(x:xs,y:ys))               by(unzip.2)
+zip (x:xs) (scd(x:xs,y:ys))                          by(frt)
+zip (x:xs) (y:ys)                                    by(scd)
+(x,y) : zip xs ys                                    by(zip.1)
+
+--I don't feel this is quite right. Its seems like I jumped a step somewhere 
+--Maybe its just different than the others
+
+-}
+
+--8.8 
+
+{-Prove
+take n xs ++ drop n xs = xs                         (hyp)
+
+take 0 _        = []                                (take.1)
+take _ []       = []                                (take.2)
+take n (x:xs)                                       (take.3)
+  | n>0         = x : take (n-1) xs                 
+  
+drop _ [] = []                                      (drop.1)
+drop n (x:xs)                                       (drop.2)
+  |n>0 = drop (n-1) xs
+  |n==0 =(x:xs)
+
+(base)
+take n [] ++ drop n []
+=[] ++ drop n []                                     by(take.2)
+=[] ++ []                                            by(drop.1)
+=[]                                                  by(++.1)
+
+[]
+
+(induction)
+take n (x:xs) ++ drop n (x:xs)
+(x: take (n-1) xs) ++ (drop n (x:xs))                by(take.2)
+(x: take (n-1) xs) ++ (drop (n-1) xs)                by(drop.2)
+x: ((take (n-1) xs) ++ (drop (n-1) xs))              by(++.2)
+x:xs                                                 by(hyp)   
+-- Can I do this or must I prove somehow that if n is defined (n+1) is defined)
+
+x:xs
+-}
+
+
