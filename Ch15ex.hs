1dcfefc7c0d1adbcf56dde4ca60c0c08b1e2ba06
Rebecca Meritz <rebecca@rebecca-Z37E.(none)>
2010-12-22 11:06:32 -0500

    Show Trees and Table

--- Ch15ex.hs
+++ Ch15ex.hs
@@ -5,13 +5,21 @@ import Types (Tree(Leaf,Node), Table)
 import MakeTree
 
 --15.1
-{-The effect of export controls can be achieved using import controls. However export controls can can provide qualified names. This high level of controls is provided so that parts of the system that depend on each other can be developed and complied more indepentently. -}
+{-The effect of export controls can be achieved using import controls.
+ However export controls can can provide qualified names.
+ This high level of controls is provided so that parts of the system
+ that depend on each other can be developed and complied more indepentently. -}
 
 --15.2
-{-It is the defalt that imported definitions are not themselves imported to prevent the accident editting of a definiation that another part of the system may depend on.-}
+{-It is the defalt that imported definitions are not themselves imported
+ to prevent the accident editting of a definiation that another part of
+ the system may depend on.-}
 
 --15.3
-{- Preventing a module from being exported of imported at all could be useful is the system if code was going known to be going to change. If this was the case it might prevent other modules from breaking when the code in the module breaks. 
+{- Preventing a module from being exported of imported at all could be
+ useful is the system if code was going known to be going to change.
+ If this was the case it might prevent other modules from breaking when
+ the code in the module breaks. 
 This could be achieved in Haskell by having the module hide itself.-}
 
 --15.4
@@ -23,7 +31,8 @@ This could be achieved in Haskell by having the module hide itself.-}
 --LRRLRLLRLRLR => Tree 2
 --LRLLLRRLLLRLR=> Tree 3
 
---Tree 1 is the most effcient bc the the most commonly used letter is maped to only on movement
+--Tree 1 is the most effcient bc the the most commonly used letter
+-- is maped to only on movement
 
 --15.6
 --battat
@@ -46,7 +55,8 @@ exam1 = [('a',1),('b',1),('c',1),('a',1),('b',1)]
 
 --15.10
 
---Can not use ordering :: a -> a -> Ordering for alphamerge because the ordering is not dependent on the ordering of the alphabet.  
+--Can not use ordering :: a -> a -> Ordering for alphamerge because
+-- the ordering is not dependent on the ordering of the alphabet.  
 
 --15.11
 
@@ -63,23 +73,26 @@ exam2 = (Node 6 (Node 3 (Leaf 'b' 1) (Leaf 'a' 2)) (Leaf 't' 2))
 Node 6
 	(Leaf 'a' 2)-}
 
-showTree :: Tree -> String 
-showTree tr = showTreeTabs tr 0
+showTree :: Tree -> IO () 
+showTree tr = putStr (showTreeTabs tr 0)
 	where
 	showTreeTabs :: Tree -> Int -> String 
-	showTreeTabs tr tab
-		| tr == (Leaf _ _) = (indent tab) ++ (show tr) ++"\n"
-		| tr == (Node _ _ _) = (showTreeTabs (left tr) (tab+1)) ++ (indent tab) ++ "Node " ++ (node tr) ++ "\n" (showTreeTabs (right tr) (tab+1))
-		where   
-		indent :: Int -> String 
-		indent num = foldr (++) "" (replicate "\t")
-		left :: Tree -> Tree
-		left (Node _ t1 _) = t1
-		right :: Tree -> Tree
-		right (Node _ _ t2) = t2
-		node :: Tree -> String
-		node (Node x _ _) = show x
---What is wrong with _ in the pattern matching?
+	showTreeTabs tr@(Leaf _ _) tab 
+		= (indent tab) ++ "Leaf " ++ (leaf tr) ++"\n"
+	showTreeTabs tr@(Node _ _ _) tab 
+		= (showTreeTabs (left tr) (tab+1)) ++ (indent tab) 
+		++ "Node " ++ (node tr) ++ "\n" ++  (showTreeTabs (right tr) (tab+1))
+	indent :: Int -> String
+	indent tabs = foldr (++) "" (replicate tabs  "\t")
+	left :: Tree -> Tree
+	left (Node _ t1 _) = t1
+	right :: Tree -> Tree
+	right (Node _ _ t2) = t2
+	leaf :: Tree -> String 
+	leaf (Leaf x y) = show x ++ " " ++ show y
+	node :: Tree -> String
+	node (Node x _ _) = show x
 
-showTable :: Table -> String
+
+showTable :: [(Char, Int)] -> IO () 
 showTable = showTree . makeTree
---
+++ Ch16ex.hs
@@ -0,0 +1,6 @@
+--The Craft of Functional Programming 
+--Ch. 16 Abstract Data Types
+
+--16.1
+
+
