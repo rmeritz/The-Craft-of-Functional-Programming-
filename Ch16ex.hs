6250607924d5e789e75152fa8bdea1038024cf19
Rebecca Meritz <rebecca@rebecca-Z37E.(none)>
2010-12-25 00:54:52 -0500

    Maybe and ADT

--- Ch16ex.hs
+++ Ch16ex.hs
@@ -11,11 +11,8 @@ It could also find/delete duplicates quicker. -}
 module Store ( Store, initial, value, update) where
 
 type Var = String
---is the best way to represent a Var? 
 
 data Store = Sto [ (Int, Var) ]
---What is the real difference between signitures and
---constructors? 
 
 exStore1 :: Store
 exStore1 = Sto [(1, "a"), (2, "b"), (3, "bat"), (3, "cat")]
@@ -54,7 +51,6 @@ secondVar s = snd (head (second s))
 
 showStore :: Store -> [ (Int, Var) ] 
 showStore (Sto s) = s
---This looks like a destructor to me?
 
 --16.2
 
@@ -71,8 +67,10 @@ s5 = Sto [(5, "h")]
 
 -- s1 == s2 == s3 /= s4 == s5 /= exStore1
 
-instance Eq Store where 
+{-
+instance Eq Store where
 (Sto sto1) == (Sto sto2) = storeEqOf sto1 sto2
+-}
 
 storeEqOf :: [(Int, Var)] -> [(Int, Var)] -> Bool 
 storeEqOf [] l2 = True
@@ -87,16 +85,7 @@ storeEqOf ((n,v):xs) ys
 
 {-Why after I define Eq over Store do I need to distiguse
 explicatly (Prelude.==) from (Store.==). Shouldn't type
-cheaking take care of that? Also why is (==) infix and
-(Prelude.==) not? -}
-
-{-Also, how do I turn spell cheak on in vim? And get better
-syntax highlighting of Haskell built in functions and type
-definitions? I tried downloading a file and putting it in
-~/.vim/syntax, but I didn't have on so I had mkdir it
-first. Didn't work. Also my maping of Esc to CapsLock was
-not permanate. Also how do I paste from the main computer
-registar again?-} 
+cheaking take care of that? -}
 
 {-There is definately a way of doing this using the list
 monad, because this defintion of Eq is close uses the idea
@@ -105,4 +94,129 @@ sto1 >>= ((Prelude.==)sto2)) -}
 
 --16.3
 
+--data Maybe a = Nothing | Just a
 
+--Use maybe to propagate errors in Store- 
+
+--For list implementation only need to change value
+
+valueE :: Store -> Var -> Maybe Int 
+valueE (Sto []) v = Nothing
+valueE (Sto ((n,vstored):sto)) v
+	| v == vstored = Just n
+	| otherwise = valueE (Sto sto) v 
+
+--For functional implemenation need to modify type too
+
+newtype StoreF = StoF ( Var -> Maybe Int ) 
+
+initialF :: StoreF
+initialF = StoF (\v -> Nothing) 
+
+valueF :: StoreF -> Var -> Maybe Int
+valueF (StoF sto) v = sto v
+
+updateF :: StoreF -> Var -> Int -> StoreF
+updateF (StoF sto) v n = StoF (\w -> if v==w then Just n else sto w)
+--not sure about my use of just in this context. 
+
+--Once I incorperate Maybe how do I get an error? 
+--Or is that just what Nothing means by defult?
+
+{-Is there a way to test it completely without
+defining an instance of a show-like function over the type? 
+What behviors should this function have?
+Is there an accepted way to show "Nothing"?-}
+
+{-Is there a syntax creating tests in Haskell similar to
+the functionality of Racket tests ? -}  
+
+--valueF (updateF initialF "a" 5) "a" => Just 5
+--valueF (updateF initialF "a" 5) "b" => Nothing
+
+--16.4
+
+{-Instead of an error provide a test that shows 
+if a variable is in a given store. 
+Also, how would the signiture need to be modifed to 
+provide a test? -} 
+
+testVal :: Store -> Var -> Bool 
+testVal (Sto []) v = False
+testVal (Sto ((n,vstored):sto)) v
+	| v == vstored = True
+	| otherwise = testVal (Sto sto) v
+
+--Is this what is meant by a test?
+
+--16.5
+
+--setAll should set every variable to the value n
+
+--Defined for Store represented by a list:
+
+--setAll :: Int -> Store
+--setAll n = Sto [( n, _ )]
+
+--Can/How can I actually do this? 
+--I don't thinks it is possible
+
+--Defined for Store represent by a function:
+
+setAllF :: Int -> StoreF
+setAllF n = (StoF (\v -> Just n)) 
+
+--valueF (setAllF 5) "a" => Just 5
+
+--16.6
+
+--Design a ADT for the library database
+{-I wanted to make it based on functions bc that is more
+confusing to me. -}
+
+type Person = String
+type Book = String
+type Borrowers = [Person]
+
+newtype Database = DB (Book -> Maybe Borrowers) 
+{-I don't think this works because I need to get
+information in both directions. (See books)
+(Book -> [Person]) and (Person -> [Book])
+How to I deal with the functions when there are
+multiple answers. (books & borrowers)
+I mean a [Person] not just a Person. 
+Is there a way I can implement this if my
+database is a function? Or does it have to be list based?-}
+
+newDB :: Database 
+newDB = DB (\b -> Nothing) 
+
+books :: Database -> Person -> Maybe [Book] 
+books (DB entries) person = --? (\p if p==person then  
+
+borrowers :: Database -> Book -> Maybe Borrowers 
+borrowers (DB entries) book = entries book
+
+borrowed :: Database -> Book -> Bool 
+borrowed (DB entries) book 
+	= Nothing /= borrowers (entries book)
+
+numBorrowed :: Database -> Person -> Int 
+numBorrowed db person = maybe 0 length (books db person)
+--I think I finally understand how to use maybe
+
+{-Not confident about below functions functioning. 
+They definately aren't dealing with multiple persons having
+the same book on loan. -}
+
+
+makeLoan :: Database -> Person -> Book -> Database
+makeLoan (DB entries) person book 
+	= DB (\b -> if b==book then Just person else entries
+	book)    
+
+returnLoan :: Database -> Person -> Book -> Database
+returnLoan (DB entries) person book 
+	= DB (\b -> if b==book then Nothing else entries book)
+
+	
